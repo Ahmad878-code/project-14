1b3b21af588e5d251897aaf9dc91d2b452a024bf
Ahmad878-code <affan.grewal73@gmail.com>
2026-01-17 16:41:58 +0500

    circumference

---
+++ app.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+
+def find_largest(num1, num2, num3):
+    if (num1 > num2) and (num1 > num3):
+        largest = num1
+    elif (num2 > num1) and (num2 > num3):
+        largest = num2
+    else:
+        largest = num3
+    #return largest
+    print("The largest number is", largest)
+find_largest(num1,num2,num3)
---
+++ circumference.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+radius = int(input("Enter radius:"))
+circumference = 2 * pi * radius
+print("The circumference of circle is:", circumference)
